0c728593780508eafd9fab39f51bd34133f546ae
Yuka <85838049+yuka-xxx@users.noreply.github.com>
2021-07-05 17:40:34 +0900

    コード整理

--- jsr/3/Ray.js
+++ jsr/3/Ray.js
@@ -4,7 +4,6 @@ export const Ray = type()
 export const ray = (endpoint, direction) => construct(Ray, p => p.ray(endpoint, direction))
 
 export const cata = f => r => r.match({ ray:(O, D) => f(O)(D) })
-export const cataR = flip(cata)
 
 export const endpoint = cata(O => _ => O)
 export const direction = cata(_ => D => D)
--- jsr/3/Sphere.js
+++ jsr/3/Sphere.js
@@ -4,7 +4,6 @@ export const Sphere = type()
 // sphere :: (Color, float, Vec3) -> Sphere
 export const sphere = (color, specular, radius, center) => construct(Sphere, p => p.sphere(color, specular, radius, center))
 export const cata = f => s => s.match({ sphere:(k, s, r, c) => f(k)(s)(r)(c) })
-export const cataR = flip(cata)
 
 export const color = cata(k => _ => _ => _ => k)
 export const specular = cata(_ => s => _ => _ => s)
